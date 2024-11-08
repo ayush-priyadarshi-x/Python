687fa725fa02573450befe92a878186175fc1890
Ayush Priyadarshi <priyadarshiayush1304@gmail.com>
2024-11-08 19:23:07 +0545

    Learned chapter 6, Conditional statements - LearnPython

---
+++ LearnPython/Chapter_6/conditional.py
@@ -0,0 +1,11 @@
+a = int(input("Enter you age : "))
+
+if(a>=18): 
+    print("You are above the age of concent. ")
+    print("Good for you ")
+elif(a<18 and a>0): 
+    print("You are not above the age of concent. ")
+    print("Not Good for you ")
+
+else: 
+    print("There was some error. ")
---
+++ LearnPython/Chapter_6/multiple_if_statement.py
@@ -0,0 +1,9 @@
+a = int(input("Enter the age : "))
+    
+if(a>=18): 
+        print("Allowed. ")
+
+elif(a<18 and a>0): 
+        print("Not Allowed. ")
+else: 
+        print("The entered age is not correct. ")
