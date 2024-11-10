ff29f4882a31cbeb22ce922a9ca40a8aa0a72a62
Ayush Priyadarshi <priyadarshiayush1304@gmail.com>
2024-11-10 08:35:37 +0545

    Learned Chapter-7 - LearnPython

---
+++ LearnPython/Chapter_7/forLoopIterate.py
@@ -0,0 +1,13 @@
+l = [1, 2, 3, 4, 5, 6]
+
+for i in l: 
+    print(f"list :  {i}")
+
+i = (1, 2, 3, 4, 5, 6)
+
+for i in l: 
+    print(f"tuple :  {i}")
+
+s = "Ayush"
+for i in s: 
+    print(f"string : {i}")
---
+++ LearnPython/Chapter_7/forLoops.py
@@ -0,0 +1,3 @@
+l = [1, "Harry", False, "This ", "Rohan "]
+for i in range(len(l)): 
+    print(l[i])
