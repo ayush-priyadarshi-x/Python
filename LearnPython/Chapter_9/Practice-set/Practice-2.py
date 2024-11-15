f0de67976f091f636bbf06762e7a756773960ec2
Ayush Priyadarshi <priyadarshiayush1304@gmail.com>
2024-11-15 08:49:29 +0545

    Fixed issues in problem 2 - LearnPython

--- LearnPython/Chapter_9/Practice-set/Practice-2.py
+++ LearnPython/Chapter_9/Practice-set/Practice-2.py
@@ -1,4 +1,5 @@
 import random
+import time
 def game(): 
     score = int(input("Enter you number. "))
     score *= random.randint(1, 100)
@@ -12,12 +13,17 @@ with open("highScore.txt", "r+") as hc:
         hc.write("0")
         hc.seek(0)
     getScore = game()
+    print(f"Your score is : {getScore}")
+    time.sleep(2)
     previousScore = int(content)
     if previousScore>getScore: 
         hc.seek(0)
-        print(f"The score {previousScore} is greater than {getScore}")
+        print("Previous score was greater")
+        print(f"Previous score : {previousScore} \n You Score : {getScore}")
     else: 
         hc.seek(0)
         print(f"The score {previousScore} is greater than {getScore}")
         hc.write(f"{getScore}")
+        print(f"Your score was greater")
+        print(f"Previous score : {previousScore} \n You Score : {getScore}")
 
