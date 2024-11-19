55dde52bc594f8ca16646cf465ea39ce5e24a247
Ayush Priyadarshi <priyadarshiayush1304@gmail.com>
2024-11-19 20:25:39 +0545

    Completed Practice-1 Chapter 10 - LearnPython

---
+++ LearnPython/Chapter_10/Practice-set/Practice-1.py
@@ -0,0 +1,12 @@
+class Programmar: 
+    company: "Microsoft"
+    def __init__(self, name, role, address):
+        self.name = name
+        self.role = role
+        self.address = address
+        print(f"Successfully made a class for : {self.name}! ")
+
+        
+
+ayush = Programmar("Ayush", "Investor", "Nepal")
+harry = Programmar("Harry", "Manager", "India")
