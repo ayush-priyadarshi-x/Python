462a35985d3b84ca7deaeb41264b0087100fd0f4
Ayush Priyadarshi <priyadarshiayush1304@gmail.com>
2024-11-19 21:05:43 +0545

    Completed Practice-4 Chapter 10 - LearnPython

---
+++ LearnPython/Chapter_10/Practice-set/Practice-4.py
@@ -0,0 +1,22 @@
+import math
+class Calculator: 
+    def __init__(self, number):
+        self.number = number if number>0 else 0 
+
+    def square(self): 
+       sqr = self.number*self.number
+       return sqr
+    def cube(self): 
+      cube = self.number*self.number*self.number
+      return cube
+    def squareRoot(self): 
+        return math.sqrt(self.number)
+    @staticmethod
+    def greet(): 
+       return "Hello There! "
+        
+
+
+myNum = Calculator(12)
+print(f"Greet : {myNum.greet()}")
+print(f"The square : {myNum.square()} \n Cube : {myNum.cube()} \n Square root : {myNum.squareRoot()}")
