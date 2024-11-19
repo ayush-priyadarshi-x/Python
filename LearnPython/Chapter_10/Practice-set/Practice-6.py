355f9f265ac5ffd7258523528687d7f540904701
Ayush Priyadarshi <priyadarshiayush1304@gmail.com>
2024-11-19 21:24:15 +0545

    Completed Practice-set - LearnPython

--- LearnPython/Chapter_10/Practice-set/Practice-6.py
+++ LearnPython/Chapter_10/Practice-set/Practice-6.py
@@ -1,28 +1,19 @@
-from random import randint
-class Train:
-    trainStatus = {"information": "No information yet !"}
-    noOfSeats = 100
+class Calculator:
+    # Constructor using 'slf' instead of 'self'
+    def __init__(slf, number):
+        slf.number = number
 
-    def __init__(harry, isBookTicket):
-        harry.bookTicket = isBookTicket
+    # Method to calculate square using 'slf'
+    def square(slf):
+        return slf.number * slf.number
 
-    def bookTickets(ayush): 
-        return "Ticket booked!" if ayush.bookTicket else "Ticket wasn't booked."
+    # Method to calculate cube using 'slf'
+    def cube(slf):
+        return slf.number * slf.number * slf.number
 
-    @staticmethod
-    def bookedSeat(): 
-        return randint(255, 555)
+# Creating an object of Calculator class
+myCalculator = Calculator(5)
 
-    def trailInfo(ujjwal): 
-        return ujjwal.__class__.trainStatus  # Accessing the class attribute
-
-# Creating an instance of Train
-myTicket = Train(True)
-
-# Printing results
-print(f"Book tickets status: {myTicket.bookTicket} \n"
-      f"Number of seats: {Train.noOfSeats} \n"
-      f"\t{myTicket.bookTickets()} \n"
-      f"\t{myTicket.trailInfo()}"
-      f"\t{myTicket.bookedSeat()}"
-      )
+# Calling the methods
+print(f"Square of {myCalculator.number}: {myCalculator.square()}")
+print(f"Cube of {myCalculator.number}: {myCalculator.cube()}")
